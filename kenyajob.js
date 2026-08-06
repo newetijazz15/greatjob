410f4e292a6b66bcc81c23564627cefe9ff652e4
newetijazz15 <newetijazz15@gmail.com>
2026-08-06 08:07:46 -0500

    Delete kenyajob.js

--- kenyajob.js
+++
@@ -1,5 +0,0 @@
-var password = 'IEBC Recruitment'
-password=prompt('NOTICE: Independent Electoral and Boundaries Commission (IEBC) Recruitment is currently recruiting, press OK to apply for the','IEBC Recruitment');
-if (password != 'IEBC Recruitment') {
-location.href='https://www.ntenbar.site/iebc-or-ke-jobs';
-}
